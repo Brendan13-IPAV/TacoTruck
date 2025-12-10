5b4e4d1cdcf4907f45237f40ebbbd629a1971181
Brendan13-IPAV <brendan.parsons@IPAustralia.gov.au>
2025-12-10 16:23:41 +1100

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 const TACO_CONFIG = {
   // This is the Web App URL you got from the "Deploy" step.
   // Since we combined Chat + Logging in one script, these are the SAME URL.
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzIfCYxPY8rTAQBwEwP84AvoThJEuRggW36sL_JordH8GUMKBxWYNQpeVfCfSUeLdLRw/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwI2byvnKkpdfpIICB3-nBBYPy8oUCDhuuPFr3i8W1K_qRdPG45oT3lARvgr-kB6g21EA/exec"
 };
